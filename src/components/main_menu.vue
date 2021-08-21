ba7e80be0cdcaecab837032277c527bd39c84327
Nick Nizovtsev <nizovtsevnv@gmail.com>
2021-08-21 23:44:10 +0400

    Add initial files

---
+++ src/components/main_menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-list>
+    <q-item clickable tag="a" to="/settings">
+      <q-item-section avatar>
+        <q-icon color="primary" name="settings" />
+      </q-item-section>
+      <q-item-section>Настройки</q-item-section>
+    </q-item>
+
+    <q-item clickable tag="a" to="/about">
+      <q-item-section avatar>
+        <q-icon color="primary" name="info" />
+      </q-item-section>
+      <q-item-section>О проекте</q-item-section>
+    </q-item>
+  </q-list>
+</template>
+
+<script>
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({})
+</script>
