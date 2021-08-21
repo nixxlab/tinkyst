1eacdfd4504814f3e79d5147646446d319ba4c3d
Nick Nizovtsev <nizovtsevnv@gmail.com>
2021-08-21 22:10:41 +0400

    Add 404 page

---
+++ src/pages/error/404.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="bg-dark flex flex-center fullscreen q-pa-md text-center text-secondary">
+    <div class="q-gutter-md">
+      <div style="font-size: 10vh">
+        {{ $t('error') }}
+      </div>
+
+      <div style="font-size: 3vh; opacity:.4">
+        {{ $t('page_not_found') }}
+      </div>
+
+      <q-btn
+        @click="$router.go(-1)"
+        color="secondary"
+        :label="$t('go_back')"
+        size="lg"
+        text-color="dark"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Error404'
+})
+</script>
